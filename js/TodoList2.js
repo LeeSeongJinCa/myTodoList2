a5838fd894c58fda0ecd5cac6f046d671bc8e0d3
이성진 <chlskatnr1@naver.com>
2019-08-19 13:22:26 +0900

    [Add] HTML, CSS half finish
    중간 점검

---
+++ js/TodoList2.js
@@ -0,0 +1,69 @@
+/* <ul class="list1">
+    <li class="circle">
+        <div class="in_circle checked"></div>
+    </li>
+    <li class="list_title">
+        qwertasdfzxcvqwerasdfzxcv
+        qwertasdfzxcvqwerasdfzxcv
+        qwertasdfzxcvqwerasdfzxcv
+        qwertasdfzxcvqwerasdfzxcv
+        qwertasdfzxcvqwerasdfzxcv
+        qwertasdfzxcvqwerasdfzxcv
+        qwertasdfzxcvqwerasdfzxcv
+    </li>
+    <li class="trash">
+        <img class="trash_img" src="img/delete.svg" alt="trash">
+    </li>
+</ul> */
+
+let main = document.getElementById('main');
+
+const createList = function() {
+    let ul = document.createElement('ul');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
