8693b8463771dca60cf6c9c19339f6984467e01b
이성진 <chlskatnr1@naver.com>
2019-08-23 20:04:43 +0900

    [Add] show item number, clear list
    남은 항목 개수, 리스트 전체 삭제 기능 추가

--- js/TodoList2.js
+++ js/TodoList2.js
@@ -2,27 +2,25 @@
 ! 기능 개요
 * 1. 리스트 추가
 * 2. 리스트에 완료기능, 삭제기능, 수정기능 추가
+* 3. 남은 리스트 개수, 전체 리스트 삭제기능 추가
 */
 
 /*
 *1번 기능 시작*/
 let obj = [];
+let checkObj = [];
 let main = document.getElementById('main');
 let input = document.getElementById('input');
 let cnt = Object.keys(obj).length;
 
-const createList = () => {
+const createList = function () {
     if (obj[cnt]["status"] == false) {
         return;
     }
     makeList();
     listOption();
 }
-const rendering = () => {
-    var list_text = document.getElementsByClassName('list_title_inner');
-    list_text[i].innerHTML = obj[i].text;
-}
-const makeList = () => {
+const makeList = function () {
     let main = document.getElementById('main');
     let list = `<ul class="list" id="${cnt}">
         <li class="circle">
@@ -30,14 +28,14 @@ const makeList = () => {
         </li>
         <li class="list_title" id="${cnt}">
             <input readOnly type="text" class="list_title_inner" value="${obj[cnt]["text"]}">
-        </li>
+            </li>
         <li class="trash" id="${cnt}">
             <img src="img/delete.svg" class="trash_img">
         </li>
     </ul>`
     main.insertAdjacentHTML("beforeend", list);
 }
-const userInput = () => {
+const userInput = function () {
     var value = document.getElementById('input').value;
     if (value.trim() == "") {
         alert('Please input enything');
@@ -45,6 +43,7 @@ const userInput = () => {
         addTodoInObj(value);
         createList();
         cnt++;
+        itemLeftCheck();
     }
     document.getElementById('input').value = "";
     document.getElementById('input').focus();
@@ -52,21 +51,18 @@ const userInput = () => {
 const addTodoInObj = (v) => {
     obj[cnt] = { "text": v };
 }
-input.addEventListener('click', inputOnKeyUp);
-const inputOnKeyUp = (el) => {
-    el.onkeyup = () => {
-        if (window.event.keyCode == 13) {
-            userInput();
-        }
+input.onkeyup = () => {
+    //* 키 코드 받아오는 방법 -> https://cofs.tistory.com/12
+    if (window.event.keyCode == 13) {
+        userInput();
     }
 }
-
 /*
 *1번 기능 끝*/
 
 /*
 *2번 기능 시작*/
-const listOption = () => {
+function listOption() {
     let circle = document.querySelectorAll('.circle');
     let trash = document.querySelectorAll('.trash');
     let listTitle = document.querySelectorAll('.list_title');
@@ -77,40 +73,78 @@ const listOption = () => {
         listTitle[i].addEventListener('focusout', focusOut);
     }
 }
-const circleToggle = () => {
+function circleToggle() {
     this.classList.toggle('circle_bg');
 }
-const removeList = () => {
-    var p = this.parentElement;
-    var pp = p.parentElement;
+function removeList() {
+    let p = this.parentElement;
+    let pp = p.parentElement;
     pp.removeChild(p);
     statusObj(this);
+    itemLeftCheck();
 }
-const statusObj = (el) => {
-    var id = el.getAttribute('id');
+function statusObj(el) {
+    let id = el.getAttribute('id');
     obj.splice(id, 1);
+    cnt--;
+    updateId();
 }
-const focusIn = () => {
-    var list_title_inner = document.querySelectorAll('.list_title_inner');
-    var id = this.getAttribute('id');
-    list_title_inner[id].parentElement.classList.add("editing");
-    list_title_inner[id].removeAttribute("readOnly");
-
-    editOnKeyUp(list_title_inner, id);
-}
-const editOnKeyUp = (el, tag, id) => {
-    el.onkeyup = () => {
-        if (window.event.keyCode == 13) {
-            tag[id].parentElement.classList.remove("editing");
-            tag[id].readOnly = "true";
-        }
+function updateId() {
+    let list_title_inner = document.querySelectorAll('.list_title_inner');
+    let list_title = document.querySelectorAll('.list_title');
+    let trash = document.querySelectorAll('.trash');
+    for (var i = obj.length - 1, j = 0; i >= 0; i-- , j++) {
+        list_title_inner[j].parentElement.setAttribute('id', j);
+        list_title[j].parentElement.setAttribute('id', j);
+        trash[j].setAttribute('id', j);
     }
 }
-const focusOut = () => {
-    var list_title_inner = document.querySelectorAll('.list_title_inner');
-    var id = this.getAttribute('id');
+function focusIn() {
+    let list_title_inner = document.querySelectorAll('.list_title_inner');
+    let id = this.getAttribute('id');
+    list_title_inner[id].parentElement.classList.add("editing");
+    list_title_inner[id].removeAttribute("readOnly");
+    list_title_inner[id].addEventListener('keyup', () => {
+        if (window.event.keyCode == 13) {
+            list_title_inner[id].parentElement.classList.remove("editing");
+            list_title_inner[id].readOnly = "true";
+        }
+    })
+}
+function focusOut() {
+    let list_title_inner = document.querySelectorAll('.list_title_inner');
+    let id = this.getAttribute('id');
     list_title_inner[id].parentElement.classList.remove("editing");
     list_title_inner[id].readOnly = "true";
 }
+function changeListCheck() {
+    let list_title_inner = document.querySelectorAll('.list_title_inner');
+    for (var i = 0; i < list_title_inner.length; i++) {
+        checkObj[i] = list_title_inner[i].value;
+        obj[i]["text"] = checkObj[i];
+    }
+}
 /*
 *2번 기능 끝*/
+
+/*
+*3번 기능 시작*/
+window.onload = () => {
+    itemLeftCheck();
+}
+function itemLeftCheck() {
+    let item_left = document.getElementById('item_left_number');
+    item_left.innerHTML = cnt;
+}
+document.getElementById('clear_list').onclick = () => {
+    if (confirm('Are you sure?')) {
+        main.innerHTML = "";
+        cnt = 0;
+        obj = [];
+        itemLeftCheck();
+    } else {
+        return;
+    }
+}
+/*
+*3번 기능 끝*/
